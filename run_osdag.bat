87b6e5fdc5318abca4a31fc9acda7020198cafe7
lakshanashreee <lakshanashree.s.2023.cse@ritchennai.edu.in>
2025-03-11 18:07:47 +0530

    Update [automation, iterate once] run_osdag.bat

--- run_osdag.bat
+++ run_osdag.bat
@@ -7,16 +7,8 @@ set ERROR_LOG="error_log.txt"
 REM Clear previous error log
 if exist %ERROR_LOG% del %ERROR_LOG%
 
-REM Loop through all OSI files in the directory
-for %%F in (*.osi) do (
-    echo Validating: %%F >> %ERROR_LOG%
-    
-    REM Run Osdag CLI tool (Module Extraction)
-    python -m osdag.cli_shell find-module -i "%%F" >> %ERROR_LOG% 2>&1
-
-    REM Run OSI file validation script
-    python validate_osi.py "%%F" >> %ERROR_LOG% 2>&1
-)
+REM Run OSI file validation script once for all files
+python validate_osi.py >> %ERROR_LOG% 2>&1
 
 REM Display error log
 type %ERROR_LOG%
